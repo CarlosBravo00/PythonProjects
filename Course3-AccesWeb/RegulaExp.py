4384be5dd0dc83dab64158e2358d52d1dd81dc55
Carlos Bravo <carlosemiliobravomarquez@gmail.com>
2020-06-30 15:14:22 -0500

    Regular Expressions added

---
+++ Course3-AccesWeb/RegulaExp.py
@@ -0,0 +1,17 @@
+import re
+
+hold = dict()
+hand = open('example.txt')
+for line in hand:
+    line = line.rstrip()
+    # if re.search('^X-.*:',line) :
+    if re.search('^X-\S+?:',line) :
+        lst = line.split(':')
+        hold[lst[0]] = int(lst[1].strip())
+print (hold)
+
+
+x = 'Code 124 Attack 24 Defense 52'
+y = re.findall('[0-9]+',x)
+print(y)
+
